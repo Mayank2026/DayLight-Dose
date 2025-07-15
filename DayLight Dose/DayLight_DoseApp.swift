651266b7f44aa3fb02fe76354b66c9d81fc2b352
Mayank Verma <133039754+Mayank2026@users.noreply.github.com>
2025-07-15 11:01:24 +0530

    Initial Commit

---
+++ DayLight Dose/DayLight_DoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayLight_DoseApp.swift
+//  DayLight Dose
+//
+//  Created by Mayank Verma on 15/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DayLight_DoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
