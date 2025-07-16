ec30db209f2b197ff42b8e12ea5c131343b72bc4
Avineet Singh <avineetsingh04@gmail.com>
2025-07-16 20:56:36 +0530

    Update header text to 'DayLight Dose'
    Changed the header button label from 'SUN DAY' to 'DayLight Dose' for consistency with the app's name.

--- DayLight Dose/Sources/Views/ContentView.swift
+++ DayLight Dose/Sources/Views/ContentView.swift
@@ -239,7 +239,7 @@ struct ContentView: View {
     
     private var headerSection: some View {
         Button(action: { showInfoSheet = true }) {
-            Text("SUN DAY")
+            Text("DayLight Dose")
                 .font(.system(size: 40, weight: .bold, design: .rounded))
                 .foregroundColor(.white)
                 .tracking(2)
