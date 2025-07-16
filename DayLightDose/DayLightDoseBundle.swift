37eb22c7bf0b10f8805d29dbfb99c7450f72e0f1
Mayank Verma <133039754+Mayank2026@users.noreply.github.com>
2025-07-16 23:26:11 +0530

    Add DayLightDose widget extension and assets
    Introduces the DayLightDose widget extension with supporting Swift code, intent definition, Info.plist, and entitlements. Updates project configuration, adds asset catalogs, modifies entitlements, and sets up scheme management for the new widget.

---
+++ DayLightDose/DayLightDoseBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DayLightDoseBundle.swift
+//  DayLightDose
+//
+//  Created by Mayank Verma on 16/07/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+//@main
+//struct DayLightDoseBundle: WidgetBundle {
+//    var body: some Widget {
+//        DayLightDose()
+//    }
+//}
